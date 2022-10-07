866d805edfed401fe48ea5e7822b7c79e0e33a67
Salman Khan <111654354+sallulearningtocode@users.noreply.github.com>
2022-10-08 00:22:19 +0530

    Add files via upload

---
+++ Assignment_23.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+using namespace std;
+/*1.Program to print Hello MySirG on the Screen
+int main()
+{
+    cout<<"Hello "<<"MySirG";
+    return 0;
+}*/
+
+/*2.Program to Print Hello on the first line and MySirG on the second line using endl.
+int main()
+{
+    cout<<"Hello"<<endl<<"MySirG";
+}*/
+
+/*3.Program to calculate the sumo of two numbers.
+int main()
+{
+    int a=10,b=20,c;
+    c=a+b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is "<<c;
+}*/
+
+/*4.Program to Calculate the area of Circle
+int main()
+{
+    float radius=7.12;
+    cout<<"Area of Circle is "<<3.14*radius*radius;
+}*/
+
+/*5.Program to calculate the cuboid
+int main()
+{
+    int l,b,h;
+    cout<<"Enter Three Sides of Cuboid";
+    cin>>l>>b>>h;
+    cout<<"Volume of Cuboid is "<<l*b*h;
+}
+
+/*6.Program to Calculate average of three numbers
+int main()
+{
+    int a,b,c;
+    float avg;
+    cin>>a>>b>>c;
+    cout<<(a+b+c)/3;
+
+}*/
+
+/*6.Write a C++ Program to calculate an average of 3 numbers.
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three numbers";
+    cin>>a>>b>>c;
+    cout<<(a+b+c)/3;
+}*/
+
+/*7.Program to Calculate suare of a number
+int main()
+{
+    int a;
+    cout<<"Enter a Number";
+    cin>>a;
+    cout<<"Square is "<<a*a;
+}*/
+
+/*8 Program to Swap values of two int without using third variable
+int main()
+{
+    int a,b,c;
+    cout<<"Enter Two Numbers";
+    cin>>a>>b;
+    cout<<"Before Swapping "<<a<<" "<<b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<endl;
+    cout<<"After Swapping "<<a<<" "<<b;
+}*/
+
+/*9.Program to find maximum of two numbers.
+int main()
+{
+    int a,b;
+    cout<<"Enter Two Numbers";
+    cin>>a>>b;
+    if(a>b)
+    cout<<a<<" is greater";
+    else
+    cout<<b<<" is greater";
+}*/
+
+/*10.Program to add all the numbers of an array of size 10.
+int main()
+{
+    int sum=0,i,a[10];
+    cout<<"Enter 10 Values in array";
+    for(i=0;i<10;i++)
+    {
+        cin>>a[i];
+        sum=sum+a[i];
+    }
+    cout<<"Sum is "<<sum;
+}*/
+
+
